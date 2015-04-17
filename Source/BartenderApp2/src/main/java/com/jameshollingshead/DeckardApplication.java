938cc9fc2928500cafdc5c1c7c4943ff7a04d967
James Hollingshead <jhollingshead@Jamess-MacBook-Pro-2.local>
2015-04-17 14:09:38 -0400

    Espresso set up.
    Espresso test checked on both phone and emulator.
    Fixed issue with package names.

---
+++ Source/BartenderApp2/src/main/java/com/jameshollingshead/DeckardApplication.java
@@ -0,0 +1,7 @@
+package com.jameshollingshead;
+
+import android.app.Application;
+
+public class DeckardApplication extends Application {
+
+}
---
+++ Source/BartenderApp2/src/main/java/com/jameshollingshead/activity/DeckardActivity.java
@@ -0,0 +1,14 @@
+package com.jameshollingshead.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.jameshollingshead.R;
+
+public class DeckardActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.deckard);
+  }
+}
