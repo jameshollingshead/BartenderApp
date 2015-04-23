ade3d39ca5d646d7d162fb4103dc2c04e0d6dbe9
James Hollingshead <git@jameshollingshead.com>
2015-04-23 11:31:32 -0400

    Changed DummyModule
    changed DummyModule to use getModules() in order to populate the
    ObjectGraph instead of doing it with the object directly.

--- Source/BartenderApp2/src/main/java/com/jameshollingshead/DeckardApplication.java
+++ Source/BartenderApp2/src/main/java/com/jameshollingshead/DeckardApplication.java
@@ -14,13 +14,12 @@ public class DeckardApplication extends Application {
     @Override public void onCreate(){
         super.onCreate();
 
-        graph = ObjectGraph.create(new DummyModule());
-//        graph = ObjectGraph.create(getModules());
+        graph = ObjectGraph.create(getModules().toArray());
     }
 
-//    public List<Object> getModules(){
-//        return Arrays.asList(new DummyModule());
-//    }
+    public List<DummyModule> getModules(){
+        return Arrays.asList(new DummyModule());
+    }
 
     public void inject(Object object){
         graph.inject(object);
