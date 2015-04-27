5f710869c29d95a8664da351070d9bef61388046
James Hollingshead <git@jameshollingshead.com>
2015-04-27 11:17:44 -0400

    Moved in needed resources
    Moved files and folders into the project that the Deckard project was
    lacking.
    
    Tests green

---
+++ Source/BartenderApp2/src/androidTest/java/com/jameshollingshead/MainEspressoTest.java
@@ -0,0 +1,28 @@
+package com.jameshollingshead;
+
+import com.jameshollingshead.MainActivity;
+import com.jameshollingshead.R;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+public class MainEspressoTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainEspressoTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testActivityShouldHaveText() throws InterruptedException {
+        onView(withId(R.id.text)).check(matches(withText("Injected Foo")));
+    }
+}
