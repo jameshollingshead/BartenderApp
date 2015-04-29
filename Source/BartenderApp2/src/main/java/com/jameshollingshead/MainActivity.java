d6522f6bcca27f64d886b6bb089d47285104f879
James Hollingshead <git@jameshollingshead.com>
2015-04-29 12:30:47 -0400

    Added missing unit test
    Added missing unit test to check that the activity_main content view is
    being used by MainActivity.

--- Source/BartenderApp2/src/main/java/com/jameshollingshead/MainActivity.java
+++ Source/BartenderApp2/src/main/java/com/jameshollingshead/MainActivity.java
@@ -2,12 +2,13 @@ package com.jameshollingshead;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
 import javax.inject.Inject;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
--- Source/BartenderApp2/src/test/java/com/jameshollingshead/MainActivityTest.java
+++ Source/BartenderApp2/src/test/java/com/jameshollingshead/MainActivityTest.java
@@ -1,19 +1,33 @@
 package com.jameshollingshead;
 
+import android.app.Activity;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.robolectric.RobolectricShadowOfLevel16.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = "src/main/AndroidManifest.xml", emulateSdk = 18)
 public class MainActivityTest {
+    private MainActivity mainActivity;
 
     @Test
-    public void testSomething() throws Exception {
+    public void testMainActivityExists() throws Exception {
         assertTrue(Robolectric.buildActivity(MainActivity.class).create().get() != null);
+    }
+
+    @Test
+    public void testMainActivityContentViewIsActivityMain() throws Exception {
+        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        assertEquals(R.id.activity_main, Robolectric.shadowOf(activity).getContentView().getId());
     }
 }
