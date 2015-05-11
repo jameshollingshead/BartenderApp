bdcc008869bbfe830ebf19b6d02fe5a106f951e3
James Hollingshead <git@jameshollingshead.com>
2015-05-11 10:18:06 -0400

    Added needed espresso test

--- Source/BartenderApp2/src/androidTest/java/com/jameshollingshead/MainEspressoTest.java
+++ Source/BartenderApp2/src/androidTest/java/com/jameshollingshead/MainEspressoTest.java
@@ -5,6 +5,7 @@ import com.jameshollingshead.R;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -30,5 +31,14 @@ public class MainEspressoTest extends ActivityInstrumentationTestCase2<MainActiv
 
     public void testMainActivityWindowHasSearchLabel() throws InterruptedException {
         onView(withId(R.id.drink_search_label)).check(matches(withText("Search for drink by name")));
+    }
+
+    public void testClickingSearchButtonOnDrinkSearchPageOpensSearchResultsPage()
+            throws InterruptedException {
+
+        onView((withId(R.id.drink_search_search_button))).perform(click());
+
+        onView(withId(R.id.drink_Search_Results_TextView))
+                .check(matches(withText("This is the Drink Search Results Screen")));
     }
 }
